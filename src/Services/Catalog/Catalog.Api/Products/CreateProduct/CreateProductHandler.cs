e6c82d5e20b5b03fb79e55f76a16000b3990a4f6
AlefT94 <alef_teodoro1@hotmail.com>
2024-06-27 14:32:12 -0300

    created CQRS interfaces

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProducResult(Guid Id);
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProducResult>;
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProducResult>
+{
+    public Task<CreateProducResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
