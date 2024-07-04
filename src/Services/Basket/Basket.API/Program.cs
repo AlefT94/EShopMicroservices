b62b6b86e425ed37d22597204fb8cfc2d4ba38f7
AlefT94 <alef_teodoro1@hotmail.com>
2024-07-03 22:43:25 -0300

    feat: implemented Basket repository

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName); //Set ID as UserName
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure GHTTP request pipeline
