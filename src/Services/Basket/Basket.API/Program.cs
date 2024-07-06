63252d28ced07b774b977e471e5d716122f258a4
AlefT94 <alef_teodoro1@hotmail.com>
2024-07-05 23:20:13 -0300

    feat(cache): add cache feature to improve performance

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.Distributed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -18,9 +20,22 @@ builder.Services.AddMarten(opts =>
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName); //Set ID as UserName
 }).UseLightweightSessions();
 
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, BasketRepository>();
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //opt.InstanceName = "Basket";
+});
+
+/*builder.Services.AddScoped<IBasketRepository>(provider =>
+{
+    var basketRepository = provider.GetService<IBasketRepository>();
+    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+});*/ //Other way to register decorators beside using Scrutor
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 var app = builder.Build();
 
 // Configure HTTP request pipeline
