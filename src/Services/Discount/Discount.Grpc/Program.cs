d2f76ec94742f95e3369aea3ddc6a27c50b9070e
AlefT94 <alef_teodoro1@hotmail.com>
2024-07-13 16:36:59 -0300

    Created discount service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +9,7 @@ builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 
 if (app.Environment.IsDevelopment())
     app.MapGrpcReflectionService();
