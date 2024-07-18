fb5b21a016040cf9337396ea93bc8bc93b74c696
AlefT94 <alef_teodoro1@hotmail.com>
2024-07-18 19:53:51 -0300

    Creating Entitys and Aggregates

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Domain.Abastractions;
+
+namespace Ordering.Domain.Models;
+
+public class Customer : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Domain.Abastractions;
+
+namespace Ordering.Domain.Models;
+
+public class Product : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+}
