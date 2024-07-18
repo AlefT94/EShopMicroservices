2f91ea64e96e2db7b7c76bd485f033b210ae84fe
AlefT94 <alef_teodoro1@hotmail.com>
2024-07-17 23:50:24 -0300

    Create DDD Ordering Abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abastractions/Aggregate.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Ordering.Domain.Abastractions;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEventes = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEventes.AsReadOnly();
+
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEventes.Add(domainEvent);
+    }
+
+    public IDomainEvent[] ClearDomainEvents() 
+    {
+        IDomainEvent[] dequeuedEvents = _domainEventes.ToArray();
+        _domainEventes.Clear();
+        return dequeuedEvents;
+    }
+}
