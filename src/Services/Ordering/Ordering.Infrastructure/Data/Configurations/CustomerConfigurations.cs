c4f6b09ceaf6d0c6a63b5d13ec347d2945a3b713
AlefT94 <alef_teodoro1@hotmail.com>
2024-07-26 08:41:24 -0300

    Created initial Entity Framework Configurations

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValuesObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class CustomerConfigurations : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).HasConversion(
+            customerId => customerId.Value, //How to save the Id in the database
+            dbId => CustomerId.Of(dbId)); //How to recover the Id from the database and convert it to the correct type
+        
+        builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+        builder.Property(c => c.Email).HasMaxLength(255);
+        builder.HasIndex(c => c.Email).IsUnique();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValuesObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id).HasConversion(
+            productId => productId.Value, //How to save the Id in the database
+            dbId => ProductId.Of(dbId)); //How to recover the Id from the database and convert it to the correct type
+
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+    }
+}
