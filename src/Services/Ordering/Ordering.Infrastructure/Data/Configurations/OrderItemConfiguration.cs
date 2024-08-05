1d98e673d662d55f6bd4fc46907803f9a285545a
AlefT94 <alef_teodoro1@hotmail.com>
2024-08-05 11:51:56 -0300

    Entity Framework applied settings

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Ordering.Domain.Models;
+using Ordering.Domain.ValuesObjects;
 
 namespace Ordering.Infrastructure.Data.Configuration;
 
@@ -8,6 +9,16 @@ public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
 {
     public void Configure(EntityTypeBuilder<OrderItem> builder)
     {
-        throw new NotImplementedException();
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasConversion(
+            OrderItemId => OrderItemId.Value, //How to save the Id in the database
+            dbId => OrderItemId.Of(dbId)); //How to recover the Id from the database and convert it to the correct type
+
+        builder.HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(x => x.ProductId);
+
+        builder.Property(x => x.Quantity).IsRequired();
+        builder.Property(x => x.Price).IsRequired();
     }
 }
