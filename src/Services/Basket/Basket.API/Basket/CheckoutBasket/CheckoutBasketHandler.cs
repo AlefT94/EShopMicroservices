19890f758b5da5c30e501a51c746fa2e3c4c363f
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-09 07:35:58 -0300

    Finished Basket Integration Event implementation

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,45 @@
+﻿
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckoutBasketCommand(BasketCheckouDto BasketCheckouDto) : ICommand<CheckoutBasketResult>;
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+{
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckouDto).NotNull().WithMessage("BasketCheckouDto cannot be null");
+        RuleFor(x => x.BasketCheckouDto.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class CheckoutBasketCommandHandler 
+    (IBasketRepository repository, IPublishEndpoint publishEndpoint)
+    : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        //get existing basket with total price
+        //set total price on basketcheckout event message
+        //send basket checkout event to rabbitmq using masstransit
+        //delete the basket
+
+        var basket = await repository.GetBasket(command.BasketCheckouDto.UserName,cancellationToken);
+        if (basket == null)
+        {
+            return new CheckoutBasketResult(false);
+        }
+
+        var eventMessage = command.BasketCheckouDto.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+        await repository.DeleteBasket(command.BasketCheckouDto.UserName, cancellationToken);
+
+        return new CheckoutBasketResult(true);
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,7 @@
+using BuildingBlocks.Messaging.MassTransit;
 using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -48,6 +48,9 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     };
     return handle;
 });
+
+//Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 //Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
