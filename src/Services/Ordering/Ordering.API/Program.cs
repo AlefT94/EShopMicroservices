ecd48094e6365a853f05fbb13255ba62142d0152
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-10 22:47:25 -0300

    Created Initial Ordering integrations structure

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
 
-builder.Services.AddApplicationServices()
-    .AddApplicationServices()
+builder.Services
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices(builder.Configuration);
 
