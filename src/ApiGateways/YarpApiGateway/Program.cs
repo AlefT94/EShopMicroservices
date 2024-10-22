38f2c37154ff5033415ae4138cbcaf4701846a84
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-22 20:25:28 -0300

    Implemented Rate Limit to Ordering Service

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxyConfig"));
 
+builder.Services.AddRateLimiter(rateLimiterOption =>
+{
+    rateLimiterOption.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
+app.UseRateLimiter();
+
 app.MapReverseProxy();
+
 app.Run();
