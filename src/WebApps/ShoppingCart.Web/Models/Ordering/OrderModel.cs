6c192a04a80ec013a9b751d22f7b12267cab9406
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-24 14:24:31 -0300

    Created ShoppingCart.Web Models

---
+++ src/WebApps/ShoppingCart.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ShoppingCart.Web.Models.Ordering;
+
+public record OrderModel(Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressModel ShippingAddress,
+    AddressModel BillingAddress,
+    PaymentModel Payment,
+    OrderStatus Status,
+    List<OrderItemModel> OrderItems);
+
+public record OrderItemModel(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+public record AddressModel(string FirstName, string LastName, string EmailAddress, string AddressLine, string Country, string State, string ZipCode);
+public record PaymentModel(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+public enum OrderStatus
+{
+    Draft = 1,
+    Pending = 2,
+    Completed = 3,
+    Cancelled = 4
+}
+
+//Wrapper classes
+public record GetOrdersResponse(PaginationResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);
