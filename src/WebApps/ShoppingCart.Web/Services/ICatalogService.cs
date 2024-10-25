8ab3cba8eeb4311ba2d05d9f8d7aae8bdb05f3b7
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-25 10:08:36 -0300

    Test Pull Request

--- src/WebApps/ShoppingCart.Web/Services/ICatalogService.cs
+++ src/WebApps/ShoppingCart.Web/Services/ICatalogService.cs
@@ -5,4 +5,5 @@ public interface ICatalogService
     Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pagesize = 10);
     Task<GetProductByIdResponse> GetProduct(Guid id); 
     Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+    //Solicitar pull request
 }
