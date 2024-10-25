30ccbda80aa390cc8d45ed3122cea933fb4623ee
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-25 10:02:22 -0300

    Test Branch

--- src/WebApps/ShoppingCart.Web/Services/ICatalogService.cs
+++ src/WebApps/ShoppingCart.Web/Services/ICatalogService.cs
@@ -3,6 +3,6 @@
 public interface ICatalogService
 {
     Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pagesize = 10);
-    Task<GetProductByIdResponse> GetProduct(Guid id);
+    Task<GetProductByIdResponse> GetProduct(Guid id); 
     Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
 }
