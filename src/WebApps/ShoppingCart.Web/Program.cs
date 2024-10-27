cd0f9517c0e650d3b0b80534f37d461a2892de4e
AlefT94 <alef_teodoro1@hotmail.com>
2024-10-27 14:52:44 -0300

    Created Product Detail and Product List pages

--- src/WebApps/ShoppingCart.Web/Program.cs
+++ src/WebApps/ShoppingCart.Web/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddRefitClient<ICatalogService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
+
 builder.Services.AddRefitClient<IBasketService>()
     .ConfigureHttpClient(c =>
     {
